49cf077cb2d7c3ee6e27b8e8f98098c7a5318852
Sambit Kumar Swain <swainsambitkumar59@gmail.com>
2024-09-23 02:11:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import MyForm from './Component/MyForm';
+
+function App() {
+  return (
+    <div>
+      
+      <MyForm/>
+    </div>
+  );
+}
+
+export default App;
